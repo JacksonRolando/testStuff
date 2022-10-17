5d2b673714d05490c42104503a9d61496afeb170
Jackson Rolando <jprolando7762@gmail.com>
2022-10-17 18:14:09 -0500

    initialize repo

---
+++ content/project1.js
@@ -0,0 +1,9 @@
+function getProject1() {
+    return {
+        title: "Hey Bitchesssss",
+        body: [
+            "I'm paragraph 1, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis pulvinar turpis. Vestibulum in tincidunt dui, eget efficitur tellus. Duis fringilla, nibh id pulvinar euismod, enim mauris suscipit risus, in volutpat neque nibh sed enim. Nam interdum augue ac tristique finibus. Cras pulvinar felis sed dui posuere, ac lobortis nulla mollis. Nunc metus nisl, condimentum at mi vitae, fermentum pellentesque urna. Nam eget est sit amet enim molestie maximus vel eu quam. Integer mollis felis mi, id malesuada neque consectetur ac. Etiam quam augue, fringilla id efficitur nec, feugiat ut arcu. Praesent convallis turpis tellus, a facilisis arcu finibus non. Cras porta sit amet tortor a",
+            "I'm paragraph 2, Nam et diam arcu. Ut non sagittis felis, non condimentum nisl. Mauris eget est lacus. Aliquam erat volutpat. Aliquam et nunc leo. Sed imperdiet ultricies suscipit. Nunc imperdiet, nisi et ullamcorper pellentesque, est dui cursus enim, quis sollicitudin purus odio sed magna. Nullam laoreet enim est, a lacinia ligula pretium quis. Aliquam id nibh mauris. Phasellus vestibulum vel urna eget dapibus. Morbi faucibus vitae neque vel vulputate. Sed et quam consectetur, convallis magna in, rutrum felis. Donec eget risus et erat faucibus mattis. Fusce finibus, ipsum vitae pellentesque tempus, ipsum nulla pretium ante, eu aliquet massa nunc egestas dui. Aliquam gravida, eros vel rhon"
+        ]
+    }
+}
